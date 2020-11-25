4ec20bc0233c7e3bbe4c4a7d353b70452d585893
AR123456 <coloradoz99@gmail.com>
2020-11-25 05:00:42 -0700

    fiz buzz

---
+++ day-5-4-fizzBuzz/main.py
@@ -0,0 +1,12 @@
+#Write your code below this row 👇
+
+for number in range(1,101):
+  # need to put this at the top or it will be missed
+  if number % 3 == 0 and number % 3 == 0:
+    print("FizzBuzz")
+  elif number % 5 == 0:
+    print("Buzz")
+  elif number % 3 == 0:
+    print("Fizz")
+   else:
+    print(number) 
