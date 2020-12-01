e1933186cf0f79564dc7aeb1a1a0870aaa5f49a4
AR123456 <coloradoz99@gmail.com>
2020-11-30 19:38:49 -0700

    functions with outputs

---
+++ Day-10/day-10-functions-with-outputs-title-case/main.py
@@ -0,0 +1,20 @@
+# def format_name(f_name,l_name):
+#   f_name = input("What is your first name ?\n")
+#   l_name = input("What is your last name ?\n")
+#   print(f_name.title() + " "+l_name.title())
+# format_name("f_name","l_name")
+
+def format_name(f_name,l_name):
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+  # print(f"{formated_f_name} {formated_l_name}")
+ 
+  return f"{formated_f_name} {formated_l_name}"
+
+# formatted_string = format_name("kald","kljkl")
+# print(formatted_string)
+
+print(format_name("kald","kljkl"))
+
+output = len("adgjkdgag")
+print(output)
