373edc7f2faa0c60a7992bc434684b63a1942d4b
AR123456 <coloradoz99@gmail.com>
2020-12-11 06:12:49 -0700

    dashed line

---
+++ Day-18/day-18-dashed line/main.py
@@ -0,0 +1,22 @@
+import turtle as t
+from turtle import Screen
+
+tim = t.Turtle()
+
+########### Challenge 2 - Draw a Dashed Line ########
+
+for _ in range(10):
+    tim.pendown()
+    tim.forward(20)
+    tim.penup()
+    tim.forward(20)
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
