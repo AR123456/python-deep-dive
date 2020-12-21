64ac7a3580a5348f72c365ba0464efdebacf91e0
AR123456 <coloradoz99@gmail.com>
2020-12-21 07:35:39 -0700

    pong psudo code

---
+++ Day-22/pong-v1-psudocode/main.py
@@ -0,0 +1,27 @@
+#imports
+from turtle import Screen
+#
+import time
+# create screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+# TODO Create the screen
+# TODO Create and move paddle
+# TODO Create another paddle
+# TODO Create the ball and move it
+# TODO detect collision with the wall and bounce
+# TODO detect collision with paddle
+# TODO detect when paddle misses
+# TODO keep score
+
+screen.listen()
+
+
+
+
+
+screen.exitonclick()
