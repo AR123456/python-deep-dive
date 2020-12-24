5d98d39dca94bd567dd41988508cb97184e4556f
AR123456 <coloradoz99@gmail.com>
2020-12-24 07:03:53 -0700

    pong ball class

---
+++ Day-22/pong-v6-ball-class/main.py
@@ -0,0 +1,38 @@
+# imports
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+#  to slow ball down import time to pause
+import time
+# Create the screen
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+# create r and l paddle from the turtle class, pass in tuple for location
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+# create ball
+ball = Ball()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+screen.exitonclick()
+
+
+# TODO detect collision with the wall and bounce
+# TODO detect collision with paddle
+# TODO detect when paddle misses
+# TODO keep score
+
+
