6dc4b5e6f261a7da0ad7ff94013d6fbdd7c2740c
AR123456 <coloradoz99@gmail.com>
2020-12-29 07:01:13 -0700

    detect colision with wall

---
+++ Day-23/turtle-crossing-3-detect-colision-with-car/main.py
@@ -0,0 +1,32 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+#create player instance
+player = Player()
+# create car manager
+car_manager = CarManager()
+
+screen.listen()
+screen.onkey(player.go_up,"Up ")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    #with every screen update create a new car (0.1) secs
+    car_manager.create_car()
+    car_manager.move_cars()
+    #detect collison with care
+    for car in car_manager.all_cars:
+        if car.distance(player) <20:
+            game_is_on = False
+
+
+screen.exitonclick()
