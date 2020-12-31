98b1a2c6f61c853956fe3d92ba4b59674f0faf0e
AR123456 <coloradoz99@gmail.com>
2020-12-31 06:29:57 -0700

    move turtle up

---
+++ Day-23/turtle-crossing-v2-move-turtle-up/car_manager.py
@@ -0,0 +1,8 @@
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+#create cars that move across right to left
+# randomly generated along the y-axis
+class CarManager:
+    pass
---
+++ Day-23/turtle-crossing-v2-move-turtle-up/main.py
@@ -0,0 +1,39 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+#create player instance
+player = Player()
+
+# move player
+screen.listen()
+screen.onkey(player.move, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+
+
+#TODO Move turtle up
+# #TODO detect collision with top wall and turtle
+#  #TODO increase level when the collision occurs
+#   #TODO put turtle back at bottom edge of screen(starting position)
+
+#    #TODO create cars
+#TODO create a car that is 2 boxes in size
+#TODO randomly give it a color
+# #TODO at increments along the far left create cars space a cars width apart randomly
+#TODO move car(s) accross screen at a speed that will increase with each level up
+#TODO detect collison with turtle
+#TODO show game over
+
+
+screen.exitonclick()
