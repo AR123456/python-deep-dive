03c0508c14209753889c6c15aa2d4df1dc6a1696
AR123456 <coloradoz99@gmail.com>
2021-01-04 06:01:33 -0700

    centrall park squirrels

---
+++ Day-25/Day-25-Squirrel-data-another-way/main.py
@@ -0,0 +1,18 @@
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+
+# based on Primary Fur Color  column create a new DataFrame csv file
+# Fur Color , Count
+
+gray_squirrels_count = len(data[data["Primary Fur Color"]=="Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"]=="Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"]=="Black"])
+
+data_dict = {
+    "Fur Color":["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels_count,red_squirrels_count,black_squirrels_count]
+}
+df = pandas.DataFrame(data_dict)
+df.to_csv("annes_count.csv")
