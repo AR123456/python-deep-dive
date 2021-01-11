1bc762835ea8c66fd01569dca78fa8123710f3a8
AR123456 <coloradoz99@gmail.com>
2021-01-11 13:04:02 -0700

    tkinter pack args

---
+++ Day-27/day-27-tkinter-pack-args/main.py
@@ -0,0 +1,19 @@
+import tkinter
+#create window
+window = tkinter.Tk()
+#title bar
+window.title("My first GUI Python program ")
+#size
+window.minsize(width=500,height=300)
+
+#label
+#text and tupel font
+my_label = tkinter.Label(text="I am a label", font=("Arial",24,"bold"))
+#.pack() puts on screen and centers it
+# my_label.pack()
+#can change where it is moved too
+my_label.pack(side="left")
+
+
+# Keep window open always at the bottom
+window.mainloop()
