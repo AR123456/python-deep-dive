dc7df9bbfeb026ba678caefbca992568ccecb783
AR123456 <coloradoz99@gmail.com>
2021-01-24 05:30:00 -0700

    DYI exceptions with raise

---
+++ Day-30/day-30-DYI-exceptions-with-raise/main.py
@@ -0,0 +1,43 @@
+#Exception Handling
+
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+#Finaly runs no matter what -not often used -
+finally:
+    raise TypeError("This is an error that I made up.")
+
+#BMI Example
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    # raise is our own exception we decide the error type and the message
+    raise ValueError("Human Height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
