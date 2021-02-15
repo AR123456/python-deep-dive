79449c13d9552d70cfa997910f6133ed2ddfc4ff
AR123456 <coloradoz99@gmail.com>
2021-02-15 07:11:27 -0700

    bearer token

---
+++ Day-38/workouts-v3-add-Bearer-Token/main.py
@@ -0,0 +1,52 @@
+# imports
+import requests
+from datetime import datetime
+#vars
+APP_ID = ""
+API_KEY = ""
+
+GENDER = "female"
+WEIGHT_KG = 67
+HEIGHT_CM = 154
+AGE = 58
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = ""
+
+exercise_text = input("What exercises did you do ? \n")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+# print(result)
+################### Start of Step 4 Solution ######################
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": str(exercise["duration_min"]),
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+
+    print(sheet_response.text)
