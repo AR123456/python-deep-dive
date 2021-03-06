e1d2960953cc3d8595b5396842410b8c3d782076
AR123456 <coloradoz99@gmail.com>
2021-03-06 05:24:42 -0700

    python functions

---
+++ Day-54/hello-Flask-v4-python-functions/main.py
@@ -0,0 +1,33 @@
+# import time
+
+#
+# current_time = time.time()
+# print(current_time)
+#
+#
+# def speed_calc_decorator():
+#     pass
+#
+#
+# def fast_function():
+#     for i in range(10000000):
+#         i * i
+#
+#
+# def slow_function():
+#     for i in range(100000000):
+#         i * i
+
+# Functions inputs/fucntionality/output
+def add(n1,n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1,n2):
+    return n1 * n2
+
+def divide(n1,n2):
+    return n1/n2
+
