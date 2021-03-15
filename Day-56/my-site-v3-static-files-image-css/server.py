576dfc66360d747b1c91a00c56c842221f7dcb4c
AR123456 <coloradoz99@gmail.com>
2021-03-15 04:33:03 -0600

    css

---
+++ Day-56/my-site-v3-static-files-image-css/server.py
@@ -0,0 +1,9 @@
+# boiler plate to spin up the server
+from flask import Flask , render_template
+app = Flask(__name__)
+@app.route("/")
+def home_route():
+    return render_template("sampleCV.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
