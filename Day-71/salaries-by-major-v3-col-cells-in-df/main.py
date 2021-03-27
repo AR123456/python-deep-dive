b3c4dd49b7d1b7f2a14e7a6beadb59a6e6b690be
AR123456 <coloradoz99@gmail.com>
2021-03-27 05:03:07 -0600

    col cells in df

---
+++ Day-71/salaries-by-major-v3-col-cells-in-df/main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+df = pd.read_csv("salaries_by_college_major.csv")
+df.head()
+# print(df.shape)
+# print(df.columns)
+# print(df.isna())
+# print(df.tail())
+clean_df = df.dropna()
+# print(clean_df.tail())
+# print(clean_df['Starting Median Salary'])
+# print(clean_df['Starting Median Salary'].max())
+# print(clean_df['Starting Median Salary'].idxmax())
+# print(clean_df['Undergraduate Major'].loc[43])
+# print(clean_df['Undergraduate Major'][43])
+print(clean_df.loc[43])
+clean_df['Mid-Career 90th Percentile Salary'].max()
+clean_df['Mid-Career 90th Percentile Salary'].idxmax()
+clean_df['Undergraduate Major'].loc[17]
