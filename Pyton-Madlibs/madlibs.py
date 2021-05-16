c5ea9ef37c1885a6d6a913840019baa742c528e8
AR123456 <coloradoz99@gmail.com>
2021-05-16 06:21:00 -0600

    python mad libs

---
+++ Pyton-Madlibs/madlibs.py
@@ -0,0 +1,11 @@
+# string concatenation
+
+adj = input("Adjective: ")
+verb1= input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programing is so {adj}! " \
+         f"It makes me so excited all the time beaause I love to {verb1}.  " \
+         f"Stay hydrated and {verb2} like you are {famous_person}!"
+print(madlib)
